153ee998774d311c8b22acc5ad8fd8266111534c
aldar-boskhomdzhiev <aldar08reg@gmail.com>
2025-08-07 13:36:27 +0300

    Add files via upload

---
+++ script_and_returning.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+docker_pfx=""
+if [[ -n "$(command -v docker)" ]] && [[ -n "$(docker container ls --format 'table {{.Names}}' 2>/dev/null | grep 'ekd-postgresql')" ]]; then
+       docker_pfx="docker exec --user postgres ekd-postgresql"
+fi
+
+get_db() {
+    local db_name=$1
+    if [[ "$db_name" == 'ekd_file' ]]; then
+            db_name='ekd_file_[^proc%]'
+    fi
+
+    result=$($docker_pfx psql -A -t -c "
+    SELECT
+        CASE
+            WHEN datname ~ '_main' THEN datname
+            ELSE (SELECT datname FROM pg_database WHERE datname ~ '_$db_name' LIMIT 1)
+        END AS dn
+    FROM pg_database
+    WHERE (datname ~ '_main' OR datname ~ '_$db_name') AND datname IS NOT NULL AND datallowconn IS true LIMIT 1;")
+
+    echo "$result"
+}
+    
+$docker_pfx psql --dbname ekd_metadata -c "SET search_path to public, ekd_metadata;
+UPDATE tenant_settings  
+SET esa_auth_disabled_date = NULL,  
+   qr_auth_disabled_date = NULL  
+WHERE tenant_id = (  
+   SELECT id FROM tenant WHERE host = 'kedo.avmining.ru'  
+)  
+RETURNING tenant_id, esa_auth_disabled_date, qr_auth_disabled_date;  
+\" > result.txt 2>&1"
